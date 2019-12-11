ea8f502f02e38805190215ec44065ffc1dab76ce
Inaki Macias <inakimacias@opendeusto.es>
2019-12-11 09:56:49 +0100

    Algunos cambios

---
+++ src/dao/DataAccessObject.java
@@ -0,0 +1,18 @@
+package dao;
+
+import jdo.Usuario;
+
+public class DataAccessObject {
+	
+	public void guardarUsuario(Usuario u) {
+		
+	}
+	
+	public void borrarUsuario(Usuario u) {
+		
+	}
+	
+	public void obtenerUsuario(Usuario u) {
+		
+	}
+}
---
+++ src/jdo/Usuario.java
@@ -0,0 +1,32 @@
+package jdo;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class Usuario {
+	
+	@PrimaryKey
+	private String email = null;
+	private String password = null;
+	
+	public Usuario() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
